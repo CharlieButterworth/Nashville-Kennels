6dde1fc8aad9a550ae5cf3a1eff03356d981106b
Charles Butterworth <charlessbutterworth@gmail.com>
2020-11-23 14:53:46 -0600

    completed ch4.

--- src/components/Kennel.js
+++ src/components/Kennel.js
@@ -1,6 +1,10 @@
 import React from "react"
 import { Animal } from "./animal/Animal"
+import { Customer } from "./Customer/Customer"
+import { Employee } from "./Employee/Employee"
 import "./Kennel.css"
+import { LocationList } from "./Location/LocationList"
+import { LocationProvider } from "./Location/LocationProvider"
 
 // This is a single line function. It is not returning HTML. It is JSX
 
@@ -18,7 +22,30 @@ export const Kennel = () => (
             <Animal />
             <Animal />
             <Animal />
+            <Animal />
         </article>
-    
+
+        <h2>Employees</h2>
+        <div className="employees">
+        <Employee />
+        <Employee />
+        </div>
+
+        <h2>Locations</h2>
+        <LocationProvider>
+        <LocationList />
+        </LocationProvider>
+        
+        <h2>Customers</h2>
+        <div className="customers">
+        <Customer />
+        <Customer />
+        <Customer />
+        <Customer />
+        </div>
+
+
+
+
     </>
 )
