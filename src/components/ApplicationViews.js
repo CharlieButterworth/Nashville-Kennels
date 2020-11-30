08783e58f56d5f17205f5aad1dc06d027b621fcb
Charles Butterworth <charlessbutterworth@gmail.com>
2020-11-30 15:27:41 -0600

    everything is working for ch11

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,84 +1,78 @@
-import React from "react"
-import { Route } from "react-router-dom"
-import { LocationProvider } from "./Location/LocationProvider"
-import { AnimalProvider } from "./animal/AnimalProvider"
-import { LocationList } from "./Location/LocationList"
-import { AnimalList } from "./animal/AnimalList"
-import { CustomerProvider } from "./Customer/CustomerProvider"
-import { CustomerList } from "./Customer/CustomerList"
-import { EmployeeProvider } from "./Employee/EmployeeProvider"
-import { EmployeeList } from "./Employee/EmployeeList"
-import { EmployeeForm } from "./Employee/EmployeeForm"
-import { AnimalForm } from "./animal/AnimalForm"
-import { EmployeeDetail } from "./Employee/EmployeeDetail"
+import React from "react";
+import { Route } from "react-router-dom";
+import { LocationProvider } from "./Location/LocationProvider";
+import { AnimalProvider } from "./animal/AnimalProvider";
+import { LocationList } from "./Location/LocationList";
+import { AnimalList } from "./animal/AnimalList";
+import { CustomerProvider } from "./Customer/CustomerProvider";
+import { CustomerList } from "./Customer/CustomerList";
+import { EmployeeProvider } from "./Employee/EmployeeProvider";
+import { EmployeeList } from "./Employee/EmployeeList";
+import { EmployeeForm } from "./Employee/EmployeeForm";
+import { AnimalForm } from "./animal/AnimalForm";
+import { EmployeeDetail } from "./Employee/EmployeeDetail";
 
 export const ApplicationViews = (props) => {
-    return (
-        <>
-            <LocationProvider>
-                {/* Render the location list when http://localhost:3000/ */}
-                <Route exact path="/">
-                    <LocationList />
-                </Route>
-            </LocationProvider>
+  return (
+    <>
+      <LocationProvider>
+        {/* Render the location list when http://localhost:3000/ */}
+        <Route exact path="/">
+          <LocationList />
+        </Route>
+      </LocationProvider>
 
-           
-           
-           
-           
-           {/* HERE ARE THE ANIMALS */}
-           
-           
-            <AnimalProvider>
-                <LocationProvider>
-                    <CustomerProvider>
-                {/* Render the animal list when http://localhost:3000/animals */}
-                <Route exact path="/animals" render={
-                    props => <AnimalList {...props} />
-                } />
-                <Route exact path="/animals/create" render={
-                    props => <AnimalForm {...props} />
-                } />
-                    
-                 </CustomerProvider>
-                </LocationProvider>
-            </AnimalProvider>
+      {/* HERE ARE THE ANIMALS */}
 
-           
-           
-           
-           
-           
-            <CustomerProvider>
-                {/* Render the animal list when http://localhost:3000/animals */}
-                <Route path="/customers">
-                    <CustomerList />
-                </Route>
-            </CustomerProvider>
+      <AnimalProvider>
+        <LocationProvider>
+          <CustomerProvider>
+            {/* Render the animal list when http://localhost:3000/animals */}
+            <Route
+              exact
+              path="/animals"
+              render={(props) => <AnimalList {...props} />}
+            />
+            <Route
+              exact
+              path="/animals/create"
+              render={(props) => <AnimalForm {...props} />}
+            />
+          </CustomerProvider>
+        </LocationProvider>
+      </AnimalProvider>
 
+      <CustomerProvider>
+        {/* Render the animal list when http://localhost:3000/animals */}
+        <Route path="/customers">
+          <CustomerList />
+        </Route>
+      </CustomerProvider>
 
+      {/* CREATE NEW EMPLOYEE */}
 
-
-            {/* CREATE NEW EMPLOYEE */}
-
-            <EmployeeProvider>
-                <LocationProvider>
-                <AnimalProvider>
-                {/* Render the animal list when http://localhost:3000/animals */}
-                <Route exact path="/employees" render={
-                    props => <EmployeeList {...props} />
-                } />
-                <Route exact path="/employees/create" render={
-                    props => <EmployeeForm {...props} />
-                } />
-                {/* New route for showing employee details */}
-            <Route path="/employees/:employeeId(\d+)" render={
-                props => <EmployeeDetail {...props} />
-            } />
-
-                    </AnimalProvider>
-                </LocationProvider>
-            </EmployeeProvider>
-        </>
-    )
-}
+      <EmployeeProvider>
+        <LocationProvider>
+          <AnimalProvider>
+            {/* Render the animal list when http://localhost:3000/animals */}
+            <Route
+              exact
+              path="/employees"
+              render={(props) => <EmployeeList {...props} />}
+            />
+            <Route
+              exact
+              path="/employees/create"
+              render={(props) => <EmployeeForm {...props} />}
+            />
+            {/* New route for showing employee details */}
+            <Route
+              path="/employees/:employeeId(\d+)"
+              render={(props) => <EmployeeDetail {...props} />}
+            />
+          </AnimalProvider>
+        </LocationProvider>
+      </EmployeeProvider>
+    </>
+  );
+};
