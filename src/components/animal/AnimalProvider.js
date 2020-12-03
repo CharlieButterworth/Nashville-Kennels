993f704a98587efa81f8324ef90c01c58620db50
Charles Butterworth <charlessbutterworth@gmail.com>
2020-12-03 17:36:45 -0600

    completed ch14

--- src/components/animal/AnimalProvider.js
+++ src/components/animal/AnimalProvider.js
@@ -35,6 +35,14 @@ export const AnimalProvider = (props) => {
             .then(res => res.json())
     }
 
+    const releaseAnimal = animalId => {
+    return fetch(`http://localhost:8088/animals/${animalId}`, {
+        method: "DELETE"
+    })
+        .then(getAnimals)
+}
+  
+
     /*
         You return a context provider which has the
         `locations` state, the `addLocation` function,
@@ -44,7 +52,7 @@ export const AnimalProvider = (props) => {
    return (
     <AnimalContext.Provider value={
       {
-        animals, addAnimals, getAnimals, getAnimalById, searchTerms, setTerms
+        animals, addAnimals, getAnimals, getAnimalById, searchTerms, setTerms, releaseAnimal
       }
     }>
       {props.children}
